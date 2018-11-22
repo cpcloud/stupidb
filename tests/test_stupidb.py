2359ec9cc49d0ab1e05c5e15e61631b78ca6c026
Phillip Cloud <cpcloud@gmail.com>
2018-11-22 09:58:13 -0500

    Remove click

--- tests/test_stupidb.py
+++ tests/test_stupidb.py
@@ -5,8 +5,6 @@
 
 import pytest
 
-from click.testing import CliRunner
-
 from stupidb import stupidb
 from stupidb import cli
 
@@ -16,6 +14,7 @@ def response():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
+
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
@@ -25,14 +24,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'stupidb.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
