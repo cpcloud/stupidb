fc1d254e20267ff32b718f606c308de7a1d6d088
Phillip Cloud <cpcloud@gmail.com>
2018-12-31 12:54:22 -0500

    join from sqlite for comparison

---
+++ tests/test.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS t;
+CREATE TABLE t (
+  z TEXT,
+  a INTEGER,
+  b INTEGER,
+  e INTEGER
+);
+INSERT INTO t VALUES
+  ('a', 1, 2, 1),
+  ('b', 2, -1, 2),
+  ('a', 3, 4, 3),
+  ('a', 4, -3, 4),
+  ('a', 1, -3, 5),
+  ('b', 2, -3, 6),
+  ('b', 3, -3, 7);
+DROP TABLE IF EXISTS s;
+CREATE TABLE s (
+  z TEXT,
+  a INTEGER,
+  b INTEGER,
+  e INTEGER
+);
+INSERT INTO s VALUES
+  ('a', 1, 2, 1),
+  ('c', 2, -1, 2),
+  ('a', 3, 4, 3),
+  ('c', 4, -3, 4),
+  ('a', 1, -3, 5),
+  ('c', 2, -3, 6),
+  ('c', 3, -3, 7);
+
+SELECT t.a, t.b, t.e, s.z
+FROM t
+LEFT OUTER JOIN s
+USING (z);
