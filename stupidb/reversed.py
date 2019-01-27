61ad6120f6bd0b8f0a0be0549ec3ecc135dd45cc
Phillip Cloud <cpcloud@gmail.com>
2019-01-26 20:38:49 -0500

    Clean up reversed a bit

--- stupidb/reversed.py
+++ stupidb/reversed.py
@@ -6,16 +6,15 @@ T = TypeVar("T", covariant=True)
 class Reversed(Sequence[T]):
     def __init__(self, values: Sequence[T] = ()) -> None:
         self.values = values
-        self.nvalues = len(values)
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}({list(self)!r})"
 
     def __len__(self) -> int:
-        return self.nvalues
+        return len(self.values)
 
     def __getitem__(self, index):
-        nvalues = self.nvalues
+        nvalues = len(self)
         if -nvalues <= index < nvalues:
             offset = nvalues * (index >= 0) - index - 1
             return self.values[offset]
