d49579d2de425637680ef34d19a1acae38a325b6
Phillip Cloud <cpcloud@gmail.com>
2019-01-26 19:42:17 -0500

    Add zero argument aggregates

--- stupidb/aggregatetypes.py
+++ stupidb/aggregatetypes.py
@@ -7,7 +7,25 @@ from stupidb.aggregator import Aggregator
 from stupidb.typehints import Input1, Input2, Input3, Output
 
 
+class NullaryAggregate(Generic[Output], abc.ABC):
+    """An aggregate or window function with zero arguments."""
+
+    __slots__ = ()
+
+    @classmethod
+    @abc.abstractmethod
+    def prepare(
+        cls, inputs: Sequence[Tuple[()]]
+    ) -> Aggregator["NullaryAggregate", Output]:
+        ...
+
+
+NA = TypeVar("NA", bound=NullaryAggregate)
+
+
 class UnaryAggregate(Generic[Input1, Output], abc.ABC):
+    """An aggregate or window function with one argument."""
+
     __slots__ = ()
 
     @classmethod
@@ -22,6 +40,10 @@ UA = TypeVar("UA", bound=UnaryAggregate)
 
 
 class BinaryAggregate(Generic[Input1, Input2, Output], abc.ABC):
+    """An aggregate or window function with two arguments."""
+
+    __slots__ = ()
+
     @classmethod
     @abc.abstractmethod
     def prepare(
@@ -34,6 +56,10 @@ BA = TypeVar("BA", bound=BinaryAggregate)
 
 
 class TernaryAggregate(Generic[Input1, Input2, Input3, Output], abc.ABC):
+    """An aggregate or window function with three arguments."""
+
+    __slots__ = ()
+
     @classmethod
     @abc.abstractmethod
     def prepare(
