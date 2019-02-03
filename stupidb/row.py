4b267ea973ff3bdbad7713638803d985280e4fc7
Phillip Cloud <cpcloud@gmail.com>
2019-02-03 13:57:03 -0500

    Document the row module

--- stupidb/row.py
+++ stupidb/row.py
@@ -1,17 +1,44 @@
 import abc
-from typing import Any, Iterator, Mapping, NoReturn
+from typing import Any, Iterator, Mapping, NoReturn, Hashable
 
 import toolz
 
 
-class AbstractRow(Mapping[str, Any], metaclass=abc.ABCMeta):
-    def __init__(self, *pieces: Mapping[str, Any], _id: int) -> None:
-        self.pieces = pieces
+class AbstractRow(Mapping[str, Any], Hashable, abc.ABC):
+    """The base row type of StupidDB.
+
+    This is the base type of the objects that are received in most user facing
+    APIs. They behave nearly identically to a standard :class:`typing.Mapping`,
+    with the exception that they are instances of :class:`typing.Hashable` and
+    values can be accessed with square-bracket syntax as well as dot notation.
+
+    Attributes
+    ----------
+    pieces
+        One or more mappings that make up row. One for most relations, and two
+        for joins.
+    _id
+        The index of this row in a table. This a private field whose details
+        are subject to change without notice.
+
+    """
+
+    __slots__ = "pieces", "_id"
+
+    def __init__(
+        self, piece: Mapping[str, Any], *pieces: Mapping[str, Any], _id: int
+    ) -> None:
+        self.pieces = (piece,) + pieces
         self._id = _id
 
-    @abc.abstractmethod
     def __hash__(self) -> int:
         return hash(tuple(tuple(item) for item in self.data.items()))
+
+    def __eq__(self, other: Any) -> bool:
+        return self.data == getattr(other, 'data', other)
+
+    def __ne__(self, other: Any) -> bool:
+        return not (self == other)
 
     @property
     @abc.abstractmethod
@@ -47,21 +74,38 @@ class AbstractRow(Mapping[str, Any], metaclass=abc.ABCMeta):
 
 
 class Row(AbstractRow):
+    """A concrete :class:`AbstractRow` subclass for single child relations."""
+
     def merge(self, other: Mapping[str, Any]) -> "Row":
+        """Combine the :class:`typing.Mapping` `other` with this one.
+
+        Parameters
+        ----------
+        other
+            Any Mapping whose keys are instances of :class:`str`.
+
+        """
         return type(self)(
             toolz.merge(self.data, getattr(other, "data", other)), _id=self._id
         )
 
-    def __hash__(self) -> int:
-        return super().__hash__()
-
     @property
     def data(self) -> Mapping[str, Any]:
+        """Return the underlying mapping of this :class:`Row`."""
         return self.pieces[0]
 
     @classmethod
     def from_mapping(cls, mapping: Mapping[str, Any], *, _id: int) -> "Row":
-        """Construct a Row instance from any mapping with string keys."""
+        """Construct a Row instance from any mapping with string keys.
+
+        Parameters
+        ----------
+        mapping
+            Any mapping with :class:`str` keys.
+        _id
+            A new row id for the returned :class:`Row` instance.
+
+        """
         return cls(getattr(mapping, "data", mapping), _id=_id)
 
     def __repr__(self) -> str:
@@ -69,6 +113,27 @@ class Row(AbstractRow):
 
 
 class JoinedRow(AbstractRow):
+    """A concrete :class:`AbstractRow` subclass for two-child relations.
+
+    This row type is used to represent rows of a relation with two children.
+    Currently this is only used for :class:`~stupidb.stupidb.Join` relations.
+
+    .. note::
+
+       :class:`JoinedRow` is the row type yielded when iterating over a join.
+       If you want to consume the rows of a join and there are overlapping
+       column names in the left and right relations you must select from the
+       :attr:`left` and :attr:`right` attributes of instances of this class to
+       disambiguate.
+
+    Attributes
+    ----------
+    left
+        A row from the left relation
+    right
+        A row from the right relation
+    """
+
     def __init__(
         self, left: Mapping[str, Any], right: Mapping[str, Any], *, _id: int
     ) -> None:
@@ -86,7 +151,13 @@ class JoinedRow(AbstractRow):
         return self._data
 
     @classmethod
-    def from_mapping(cls, *args: Any, **kwargs: Any) -> NoReturn:
+    def from_mapping(cls, *args: Any, **kwargs: Any) -> "JoinedRow":
+        """Raise an error.
+
+        A :class:`JoinedRow` cannot be constructed from an arbitrary
+        :class:`typing.Mapping`.
+
+        """
         raise TypeError(f"from_mapping not supported for {cls.__name__!r}")
 
     def __getitem__(self, key: str) -> Any:
