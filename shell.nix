830ec2cd61d2eaad0c71118c8b146adb67d68f63
Phillip Cloud <cpcloud@gmail.com>
2021-07-19 14:25:13 -0400

    feat(dev): add the ability to use conda with nix

--- shell.nix
+++ shell.nix
@@ -6,11 +6,26 @@ let
       stupidb = ./stupidb;
     };
   };
+  condaShellRun = pkgs.writeShellScriptBin "conda-shell-run" ''
+    ${pkgs.conda}/bin/conda-shell -c "$@"
+  '';
 in
 pkgs.mkShell {
   name = "stupidb";
   shellHook = ''
     ${(import ./pre-commit.nix).pre-commit-check.shellHook}
+    ${pkgs.conda}/bin/conda-shell -c 'conda-install 2> /dev/null || true'
   '';
-  buildInputs = (with pkgs; [ git poetry graphviz gcc ]) ++ [ poetryEnv ];
+  buildInputs = (
+    with pkgs; [
+      conda
+      gcc
+      git
+      graphviz
+      poetry
+    ]
+  ) ++ [
+    poetryEnv
+    condaShellRun
+  ];
 }
