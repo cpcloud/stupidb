68bfda45fb3215c56ac25a40d76d47f20dacdc13
Phillip Cloud <cpcloud@gmail.com>
2021-07-19 16:53:29 -0400

    ci: split out conda shell env into separate shell env

--- shell.nix
+++ shell.nix
@@ -10,22 +10,35 @@ let
     ${pkgs.conda}/bin/conda-shell -c "$@"
   '';
 in
-pkgs.mkShell {
-  name = "stupidb";
-  shellHook = ''
-    ${(import ./pre-commit.nix).pre-commit-check.shellHook}
-    ${pkgs.conda}/bin/conda-shell -c 'conda-install 2> /dev/null || true'
-  '';
-  buildInputs = (
-    with pkgs; [
-      conda
-      gcc
-      git
-      graphviz
-      poetry
-    ]
-  ) ++ [
-    poetryEnv
-    condaShellRun
-  ];
+{
+  conda = pkgs.mkShell {
+    name = "stupidb-conda";
+    buildInputs = (
+      with pkgs; [
+        conda
+        git
+        poetry
+      ]
+    ) ++ [
+      condaShellRun
+    ];
+  };
+
+  dev = pkgs.mkShell {
+    name = "stupidb-dev";
+    shellHook = ''
+      ${(import ./pre-commit.nix).pre-commit-check.shellHook}
+      ${pkgs.conda}/bin/conda-shell -c 'conda-install 2> /dev/null || true'
+    '';
+    buildInputs = (
+      with pkgs; [
+        gcc
+        git
+        graphviz
+        poetry
+      ]
+    ) ++ [
+      poetryEnv
+    ];
+  };
 }
