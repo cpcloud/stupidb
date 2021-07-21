9500085bfa6e09fbb1bd264b908ac6afd4f4487d
Phillip Cloud <cpcloud@gmail.com>
2021-07-21 11:54:07 -0400

    feat(animate): add bitgraph implementation

---
+++ stupidb/bitgraph.py
@@ -0,0 +1,50 @@
+"""Implementation of a graph whose vertices are unsigned integers."""
+
+from __future__ import annotations
+
+from typing import AbstractSet, Any, Iterable, Iterator, Mapping, MutableMapping, Tuple
+
+import cytoolz as toolz
+
+from .bitset import BitSet
+
+
+class BitGraph:
+    """A graph whose vertices are unsigned integers."""
+
+    __slots__ = "_nodes", "_predecessors"
+
+    def __init__(self, nodes: Mapping[int, AbstractSet[int]]) -> None:
+        self._nodes: Mapping[int, AbstractSet[int]] = toolz.valmap(BitSet, nodes)
+        self._predecessors: MutableMapping[int, int] = {}
+
+        for parent, children in self._nodes.items():
+            for child in children:
+                self._predecessors[child] = parent
+
+    @classmethod
+    def from_vertices_and_edges(
+        cls,
+        *,
+        vertices: Iterable[int],
+        edges: Iterable[Tuple[int, int]],
+    ) -> BitGraph:
+        nodes = {vertex: BitSet() for vertex in vertices}
+        for source, dest in edges:
+            nodes[source].add(dest)
+        return cls(nodes)
+
+    @property
+    def nodes(self) -> Mapping[int, AbstractSet[int]]:
+        return self._nodes
+
+    @property
+    def predecessors(self) -> Mapping[int, int]:
+        return self._predecessors
+
+    @property
+    def in_edges(self) -> Iterator[int]:
+        return (source for source, nodes in self._nodes.items() if not nodes)
+
+    def __eq__(self, other: Any) -> bool:
+        return self.nodes == other.nodes and self.predecessors == other.predecessors
---
+++ stupidb/tests/test_bitgraph.py
@@ -0,0 +1,27 @@
+import pytest
+from bitset import BitSet
+
+from stupidb.bitgraph import BitGraph
+
+
+@pytest.fixture
+def g():
+    return BitGraph({0: {1, 2}, 1: {}, 2: {}})
+
+
+def test_from_vertices_and_edges(g):
+    assert g == BitGraph.from_vertices_and_edges(
+        vertices=[0, 1, 2], edges=[(0, 1), (0, 2)]
+    )
+
+
+def test_nodes(g) -> None:
+    assert g.nodes == {0: BitSet({1, 2}), 1: BitSet(), 2: BitSet()}
+
+
+def test_predecessors(g: BitGraph) -> None:
+    assert g.predecessors == {1: 0, 2: 0}
+
+
+def test_in_edges(g: BitGraph) -> None:
+    assert set(g.in_edges) == {1, 2}
