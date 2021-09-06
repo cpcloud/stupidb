776f930eef08d6962683a365d2e38f06919ab662
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-09-06 10:09:05 -0400

    refactor: implement a _produce method instead of __iter__ for less row munging

--- stupidb/core.py
+++ stupidb/core.py
@@ -19,7 +19,7 @@ import collections
 import functools
 import itertools
 import typing
-from typing import Any, FrozenSet, Generic, Iterable, Iterator, Mapping, NoReturn, Tuple
+from typing import Any, FrozenSet, Generic, Iterable, Iterator, Mapping, Tuple
 from typing import Union as Union_
 
 import cytoolz as toolz
@@ -52,8 +52,25 @@ class Relation(abc.ABC):
         self.partitioners: Mapping[str, PartitionBy] = {}
 
     def __iter__(self) -> Iterator[AbstractRow]:
-        """Iterate over the rows of a :class:`~stupidb.stupidb.Relation`."""
-        return (row._renew_id(id) for id, row in enumerate(filter(None, self.child)))
+        """Iterate over the rows of a :class:`~stupidb.stupidb.Relation`.
+
+        This method will reify rows with a new row identifier equal to the row number.
+
+        """
+        return (
+            Row.from_mapping(row, _id=id)
+            for id, row in enumerate(filter(None, self._produce()))
+        )
+
+    @abc.abstractmethod
+    def _produce(self) -> Iterator[AbstractRow]:
+        """Iterate over the rows of a :class:`~stupidb.stupidb.Relation`.
+
+        Specific relation should implement this without reifying the row with
+        the row identifier if possible. Reification is handled in the
+        :meth:`~stupidb.stupidb.Relation.__iter__` method.
+
+        """
 
 
 class Table(Relation):
@@ -63,6 +80,9 @@ class Table(Relation):
     def from_iterable(cls, iterable: Iterable[Mapping[str, Any]]) -> Relation:
         """Construct a :class:`~stupidb.stupidb.Table` from an iterable."""
         return cls(map(Row.from_mapping, iterable))
+
+    def _produce(self) -> Iterator[AbstractRow]:
+        return iter(self.child)
 
 
 FullProjector = Union_[Projector, WindowAggregateSpecification]
@@ -95,7 +115,7 @@ class Projection(Relation):
             if callable(projector)
         }
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         aggregations = self.aggregations
         # we need a row iterator for every aggregation to be fully generic
         # since they potentially share no structure
@@ -130,9 +150,9 @@ class Projection(Relation):
         # Use zip_longest here, because either of aggrows or projrows can be
         # empty
         return (
-            Row(toolz.merge(projrow, aggrow), _id=i)
-            for i, (aggrow, projrow) in enumerate(
-                itertools.zip_longest(aggrows, projrows, fillvalue={})
+            Row(toolz.merge(projrow, aggrow), _id=-1)
+            for aggrow, projrow in itertools.zip_longest(
+                aggrows, projrows, fillvalue={}
             )
         )
 
@@ -142,41 +162,35 @@ class Mutate(Projection):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         # reasign self.child here to avoid clobbering its iteration
         # we need to use it twice: once for the computed columns (self.child)
         # used during the iteration of super().__iter__() and once for the
         # original relation (child)
         child, self.child = itertools.tee(self.child)
         return (
-            Row.from_mapping(row, _id=i)
-            for i, row in enumerate(map(toolz.merge, child, super().__iter__()))
+            Row.from_mapping(row, _id=-1)
+            for row in map(toolz.merge, child, super()._produce())
         )
 
 
 class Aggregation(Generic[AssociativeAggregate], Relation):
-    """A relation representing aggregation of columns.
+    """A relation representing aggregation of columns."""
 
-    Attributes
-    ----------
-    aggregations
-
-    """
-
-    __slots__ = ("aggregations",)
+    __slots__ = ("metrics",)
 
     def __init__(
         self,
         child: Relation,
-        aggregations: Mapping[str, AggregateSpecification[AssociativeAggregate]],
+        metrics: Mapping[str, AggregateSpecification[AssociativeAggregate]],
     ) -> None:
         super().__init__(child)
-        self.aggregations: Mapping[
+        self.metrics: Mapping[
             str, AggregateSpecification[AssociativeAggregate]
-        ] = aggregations
+        ] = metrics
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        aggregations = self.aggregations
+    def _produce(self) -> Iterator[AbstractRow]:
+        aggregations = self.metrics
 
         # initialize aggregates
         grouped_aggs: Mapping[
@@ -196,14 +210,14 @@ class Aggregation(Generic[AssociativeAggregate], Relation):
                 inputs = (getter(row) for getter in aggregations[name].getters)
                 agg.step(*inputs)
 
-        for id, (grouping_key, aggs) in enumerate(grouped_aggs.items()):
+        for grouping_key, aggs in grouped_aggs.items():
             data = dict(grouping_key)
             data.update((name, agg.finalize()) for name, agg in aggs.items())
-            yield Row(data, _id=id)
+            yield Row.from_mapping(data)
 
 
 class Selection(Relation):
-    """A relation representing filtering rows based on a predicate.
+    """A relation of rows selected based on a predicate.
 
     Attributes
     ----------
@@ -219,11 +233,8 @@ class Selection(Relation):
         super().__init__(child)
         self.predicate = predicate
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        return (
-            row._renew_id(id)
-            for id, row in enumerate(filter(self.predicate, self.child))
-        )
+    def _produce(self) -> Iterator[AbstractRow]:
+        return filter(self.predicate, self.child)
 
 
 class GroupBy(Relation):
@@ -243,6 +254,9 @@ class GroupBy(Relation):
         super().__init__(child)
         self.partitioners = group_by
 
+    def _produce(self) -> Iterator[AbstractRow]:
+        return iter(self.child)
+
 
 class SortBy(Relation):
     """A relation representing rows of its child sorted by one or more keys.
@@ -252,30 +266,29 @@ class SortBy(Relation):
     order_by
         A callable that takes an :class:`~stupidb.row.AbstractRow` and returns
         an instance of :class:`~stupidb.protocols.Comparable`.
-    nulls
+    null_ordering
         Whether to place the nulls of a column first or last.
 
     """
 
-    __slots__ = "order_by", "nulls"
+    __slots__ = "order_by", "null_ordering"
 
     def __init__(
-        self, child: Relation, order_by: Tuple[OrderBy, ...], nulls: Nulls
+        self, child: Relation, order_by: Tuple[OrderBy, ...], null_ordering: Nulls
     ) -> None:
         super().__init__(child)
         self.order_by = order_by
-        self.nulls = nulls
+        self.null_ordering = null_ordering
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        return (
-            row._renew_id(id)
-            for id, row in enumerate(
-                sorted(
-                    self.child,
-                    key=functools.cmp_to_key(
-                        functools.partial(row_key_compare, self.order_by, self.nulls)
-                    ),
-                )
+    def _produce(self) -> Iterator[AbstractRow]:
+        return iter(
+            sorted(
+                self.child,
+                key=functools.cmp_to_key(
+                    functools.partial(
+                        row_key_compare, self.order_by, self.null_ordering
+                    )
+                ),
             )
         )
 
@@ -288,7 +301,7 @@ class Limit(Relation):
         self.offset = offset
         self.limit = limit
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         return itertools.islice(self.child, self.offset, self.offset + self.limit, None)
 
 
@@ -298,22 +311,21 @@ class Join(Relation):
     def __init__(self, left: Relation, right: Relation) -> None:
         self.grouped = itertools.groupby(
             (
-                JoinedRow(left_row, right_row, _id=i)
-                for i, (left_row, right_row) in enumerate(
-                    itertools.product(left, right)
-                )
+                JoinedRow(left_row, right_row, _id=-1)
+                for left_row, right_row in itertools.product(left, right)
             ),
             key=lambda row: row.left,
         )
-        super().__init__(row for _, rows in self.grouped for row in rows)
-
-    @classmethod
-    def from_iterable(cls, *args: Any, **kwargs: Any) -> NoReturn:
-        raise TypeError(f"from_iterable not supported for {cls.__name__} type")
+        super().__init__(
+            itertools.chain.from_iterable(rows for _, rows in self.grouped)
+        )
 
 
 class CrossJoin(Join):
     __slots__ = ()
+
+    def _produce(self) -> Iterator[AbstractRow]:
+        return iter(self.child)
 
 
 class InnerJoin(Join):
@@ -325,12 +337,8 @@ class InnerJoin(Join):
         super().__init__(left, right)
         self.predicate = predicate
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        return (
-            row._renew_id(id)
-            for id, row in enumerate(self.child)
-            if self.predicate(row.left, row.right)
-        )
+    def _produce(self) -> Iterator[AbstractRow]:
+        return (row for row in self.child if self.predicate(row.left, row.right))
 
 
 class LeftJoin(Join):
@@ -342,8 +350,7 @@ class LeftJoin(Join):
         super().__init__(left, right)
         self.predicate = predicate
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        k = 0
+    def _produce(self) -> Iterator[AbstractRow]:
         for left_row, joined_rows in self.grouped:
             matched = False
 
@@ -351,11 +358,9 @@ class LeftJoin(Join):
                 right_row = joined_row.right
                 if self.predicate(left_row, right_row):
                     matched = True
-                    yield JoinedRow(left_row, right_row, _id=k)
-                    k += 1
+                    yield JoinedRow(left_row, right_row, _id=-1)
             if not matched:
-                yield JoinedRow(left_row, dict.fromkeys(left_row), _id=k)
-                k += 1
+                yield JoinedRow(left_row, dict.fromkeys(left_row), _id=-1)
 
 
 class RightJoin(LeftJoin):
@@ -366,9 +371,9 @@ class RightJoin(LeftJoin):
     ) -> None:
         super().__init__(right, left, predicate)
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        for id, joined_row in enumerate(super().__iter__()):
-            yield JoinedRow(joined_row.right, joined_row.left, _id=id)
+    def _produce(self) -> Iterator[AbstractRow]:
+        for row in super()._produce():
+            yield JoinedRow(row.right, row.left)
 
 
 class SetOperation(Relation):
@@ -393,15 +398,10 @@ class Union(SetOperation):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        return (
-            Row.from_mapping(row, _id=id)
-            for id, row in enumerate(
-                toolz.unique(
-                    itertools.chain(self.left, self.right),
-                    key=lambda row: frozenset(row.items()),
-                )
-            )
+    def _produce(self) -> Iterator[AbstractRow]:
+        return toolz.unique(
+            itertools.chain(self.left, self.right),
+            key=lambda row: frozenset(row.items()),
         )
 
 
@@ -410,11 +410,8 @@ class UnionAll(SetOperation):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
-        return (
-            Row.from_mapping(row, _id=id)
-            for id, row in enumerate(itertools.chain(self.left, self.right))
-        )
+    def _produce(self) -> Iterator[AbstractRow]:
+        return itertools.chain(self.left, self.right)
 
 
 class IntersectAll(SetOperation):
@@ -422,18 +419,14 @@ class IntersectAll(SetOperation):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         left_set = self.itemize(self.left)
         right_set = self.itemize(self.right)
-        left_filtered = (
-            dict(row_items) for row_items in left_set if row_items in right_set
-        )
-        right_filtered = (
-            dict(row_items) for row_items in right_set if row_items in left_set
-        )
+        left_filtered = (row_items for row_items in left_set if row_items in right_set)
+        right_filtered = (row_items for row_items in right_set if row_items in left_set)
         return (
-            Row.from_mapping(row, _id=id)
-            for id, row in enumerate(itertools.chain(left_filtered, right_filtered))
+            Row.from_mapping(dict(row))
+            for row in itertools.chain(left_filtered, right_filtered)
         )
 
 
@@ -442,10 +435,10 @@ class Intersect(SetOperation):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         return (
-            Row.from_mapping(dict(row), _id=id)
-            for id, row in enumerate(self.itemize(self.left) & self.itemize(self.right))
+            Row.from_mapping(dict(row))
+            for row in self.itemize(self.left) & self.itemize(self.right)
         )
 
 
@@ -454,15 +447,12 @@ class Difference(SetOperation):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         right_set = self.itemize(self.right)
-        filtered = (
-            dict(row_items)
+        return toolz.unique(
+            Row.from_mapping(dict(row_items))
             for row_items in (tuple(row.items()) for row in self.left)
             if row_items not in right_set
-        )
-        return toolz.unique(
-            Row.from_mapping(row, _id=id) for id, row in enumerate(filtered)
         )
 
 
@@ -471,11 +461,10 @@ class DifferenceAll(SetOperation):
 
     __slots__ = ()
 
-    def __iter__(self) -> Iterator[AbstractRow]:
+    def _produce(self) -> Iterator[AbstractRow]:
         right_set = self.itemize(self.right)
-        filtered = (
-            dict(row_items)
+        return (
+            Row.from_mapping(dict(row_items))
             for row_items in (tuple(row.items()) for row in self.left)
             if row_items not in right_set
         )
-        return (Row.from_mapping(row, _id=id) for id, row in enumerate(filtered))
