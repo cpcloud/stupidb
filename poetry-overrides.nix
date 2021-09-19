cff9d1f83fdede9ad13369e7b0eb870009cff7de
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-09-19 17:25:52 -0400

    chore: bump dev deps

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -2,10 +2,15 @@
   pytest-randomly = pysuper.pytest-randomly.overrideAttrs (attrs: {
     propagatedBuildInputs = (attrs.propagatedBuildInputs or [ ])
       ++ [ pyself.importlib-metadata ];
+    postPatch = "";
   });
 
   cytoolz = pysuper.cytoolz.overridePythonAttrs (attrs: {
     nativeBuildInputs = (attrs.nativeBuildInputs or [ ])
       ++ [ pkgs.stdenv.cc ];
+  });
+
+  black = pysuper.black.overridePythonAttrs (_: {
+    dontPreferSetupPy = true;
   });
 }
