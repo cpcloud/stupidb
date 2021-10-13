dff76038609cc1826baa6a31d48973553cf9c7fa
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-10-13 09:28:40 -0400

    feat: add pretty printing functionality

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -2,4 +2,8 @@
   black = pysuper.black.overridePythonAttrs (_: {
     dontPreferSetupPy = true;
   });
+
+  tabulate = pysuper.tabulate.overridePythonAttrs (_: {
+    TABULATE_INSTALL = "lib-only";
+  });
 }
