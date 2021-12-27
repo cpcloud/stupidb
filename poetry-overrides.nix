decb77e370e70a59225c0d06a8ae8c4fc6cf8a79
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-12-27 07:23:28 -0500

    refactor: use cpcloud/flake-update-action in update-deps.yml

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,12 +1,5 @@
-{ ... }: pyself: pysuper: {
-  black = pysuper.black.overridePythonAttrs (_: {
-    dontPreferSetupPy = true;
-  });
-
+{ ... }: _: pysuper: {
   tabulate = pysuper.tabulate.overridePythonAttrs (_: {
     TABULATE_INSTALL = "lib-only";
-  });
-  typing-extensions = pysuper.typing-extensions.overridePythonAttrs (attrs: {
-    buildInputs = (attrs.buildInputs or [ ]) ++ [ pyself.flit-core ];
   });
 }
