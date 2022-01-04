71c207eb893f61061c57872b124be70c95453651
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-01-03 21:55:11 -0500

    fix: run with plain semantic-release using semantic-release/exec

--- stupidb/__init__.py
+++ stupidb/__init__.py
@@ -3,4 +3,9 @@
 from stupidb.aggregation import Window  # noqa: F401
 from stupidb.api import *  # noqa: F401,F403
 
-__version__ = "6.1.0"
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
