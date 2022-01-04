3a00195ede6cc9960babb585cea7ad4392772cfe
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-01-04 08:00:52 -0500

    chore: upload wheel to github release

--- ci/release/publish.sh
+++ ci/release/publish.sh
@@ -4,20 +4,4 @@
 
 set -euo pipefail
 
-if [ "$#" -ne "1" ]; then
-  echo "error: exactly one argument required, got: $#"
-  exit 1
-fi
-
-dry_run="$1"
-
-if [ -n "${dry_run}" ]; then
-  echo "error: dry_run not passed as argument"
-  exit 1
-fi
-
-if "$dry_run"; then
-  poetry publish --dry-run
-else
-  poetry publish
-fi
+poetry publish "$@"
