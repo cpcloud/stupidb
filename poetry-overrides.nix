2ca86b1b7ad50eb9b85f37cd7fe23299e6dfd776
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-01-21 00:50:45 +0000

    chore: add isort poetry override

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,5 +1,9 @@
-{ ... }: _: pysuper: {
+{ ... }: pyself: pysuper: {
   tabulate = pysuper.tabulate.overridePythonAttrs (_: {
     TABULATE_INSTALL = "lib-only";
+  });
+
+  isort = pysuper.isort.overridePythonAttrs (attrs: {
+    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ pyself.poetry ];
   });
 }
